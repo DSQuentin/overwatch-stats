0da0dcbd24f4d2f53f65850169adfe6cde17fba6
Quentin <dsquentin@hotmail.fr>
2022-12-21 23:04:38 +0100

    chore: config tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  mode: "jit",
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
