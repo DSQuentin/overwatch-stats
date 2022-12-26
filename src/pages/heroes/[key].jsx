49e11a71a55cf9126c2e84cfd121da80142b23d8
Quentin <dsquentin@hotmail.fr>
2022-12-26 14:44:53 +0100

    feat: add roles icons

--- src/pages/heroes/[key].jsx
+++ src/pages/heroes/[key].jsx
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable react-hooks/exhaustive-deps */
 import { getHeroData } from "@/utils/api";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -14,18 +16,16 @@ export default function HeroPage() {
       setHero(data);
     }
     fetchData();
-  }, [key]);
-
-  console.log(hero);
+  }, []);
 
   return (
     <>
       <div>
         <h1>{hero.name}</h1>
         <img src={hero.portrait} alt={hero.name} />
-        <p>{hero.description}</p>
-        <p>{hero.role}</p>
-        <p>{hero.story.summary}</p>
+        <p>Description : {hero.description}</p>
+        <p>Lieu : {hero.location}</p>
+        <p>Role : {hero.role}</p>
       </div>
       <Link href="/heroes">Retour</Link>
     </>
--- src/pages/heroes/index.jsx
+++ src/pages/heroes/index.jsx
@@ -1,34 +1,39 @@
+/* eslint-disable @next/next/no-img-element */
 import { getHeroes, getRoles } from "@/utils/api";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function HeroesPage() {
   const [heroes, setHeroes] = useState([]);
-  const [roles, setRoles] = useState([]);
   const [selectedRole, setSelectedRole] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
-      const data = await getHeroes();
-      setHeroes(data);
+      const [heroesData, rolesData] = await Promise.all([
+        getHeroes(),
+        getRoles(),
+      ]);
+      setHeroes(heroesData);
+      setRoles(rolesData);
     }
     fetchData();
   }, []);
 
-  useEffect(() => {
-    async function fetchData() {
-      const data = await getRoles();
-      setRoles(data);
-    }
-    fetchData();
-  }, []);
+  const [roles, setRoles] = useState([]);
 
   const filteredHeroes = selectedRole
     ? heroes.filter((hero) => hero.role === selectedRole)
     : heroes;
 
-  console.log(filteredHeroes);
-  console.log(selectedRole);
+  console.log(roles);
+
+  function getRoleIconUrl(roleName) {
+    const role = roles.find(
+      (r) => r.name.toLowerCase() === roleName.toLowerCase()
+    );
+    console.log(role);
+    return role ? role.icon : "";
+  }
 
   return (
     <div>
@@ -38,20 +43,38 @@ export default function HeroesPage() {
           <button
             key={role.id}
             onClick={() => setSelectedRole(role.name.toLowerCase())}
-            className={selectedRole === role.name ? "bg-gray-200" : ""}
+            className={
+              selectedRole === role.name.toLowerCase()
+                ? "bg-gray-700 mx-2 "
+                : "bg-gray-500 mx-2 "
+            }
           >
-            {role.name}
+            <img
+              src={getRoleIconUrl(role.name)}
+              className="p-4"
+              alt={role.name}
+            />
           </button>
         ))}
-        <button onClick={() => setSelectedRole(null)}>Tous</button>
+        <button
+          onClick={() => setSelectedRole(null)}
+          className={selectedRole === null ? "bg-gray-200" : ""}
+        >
+          Tous
+        </button>
       </div>
 
       <div className="flex justify-between flex-wrap">
-        {heroes.map((hero) => (
+        {filteredHeroes.map((hero) => (
           <Link key={hero.key} href={`/heroes/${hero.key}`}>
             <div>
               <p>{hero.name}</p>
               <img src={hero.portrait} alt={hero.name} />
+              <img
+                src={getRoleIconUrl(hero.role)}
+                className="bg-gray-700 p-4"
+                alt={hero.role}
+              />
             </div>
           </Link>
         ))}
