92c076f805cf08f533c1b354655ef524b7d28424
Quentin <dsquentin@hotmail.fr>
2023-01-03 15:32:56 +0100

    style: adding style to roles filters buttons

--- src/components/RolesFilter.jsx
+++ src/components/RolesFilter.jsx
@@ -7,35 +7,41 @@ export default function RolesFilter({
   setSelectedRole,
   getRoleIconUrl,
 }) {
+  let activeButton = () => {
+    return "text-black bg-gray-300  border-2 rounded-sm border-white px-8 py-2.5 text-center inline-flex items-center mr-2 mb-2 gap-2 text-lg";
+  };
+
+  let innactiveButton = () => {
+    return "text-customBlack hover:text-black bg-gray-200 hover:bg-gray-300 border-2 rounded-sm border-white px-8 py-2.5 text-center inline-flex items-center mr-2 mb-2 gap-2 text-lg";
+  };
+
   return (
-    <>
+    <div className="flex gap-3 my-8">
       <button
         onClick={() => setSelectedRole(null)}
-        className={
-          selectedRole === null
-            ? "px-4 py-2 border-2 border-customGray bg-gray-900"
-            : "px-4 py-2 border-2 border-customGray bg-gray-500"
-        }
+        className={selectedRole === null ? activeButton() : innactiveButton()}
       >
         Tous
       </button>
+
       {roles.map((role) => (
         <button
           key={role.id}
           onClick={() => setSelectedRole(role.name.toLowerCase())}
           className={
             selectedRole === role.name.toLowerCase()
-              ? "bg-gray-700 mx-2 "
-              : "bg-gray-500 mx-2 "
+              ? activeButton()
+              : innactiveButton()
           }
         >
           <img
             src={getRoleIconUrl(role.name)}
-            className="p-4"
+            className="px-2 py-2 w-10 h-auto rounded-lg bg-gray-700"
             alt={role.name}
           />
+          {role.name}
         </button>
       ))}
-    </>
+    </div>
   );
 }
